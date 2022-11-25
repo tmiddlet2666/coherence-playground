25b2179d3094303299902a3805e3ddb92caf7721
Tim Middleton <tim.middleton@oracle.com>
2022-11-25 08:30:37 +0800

    Update script to relfect change of dashboard names

--- monitoring/grafana/download-dashboards.sh
+++ monitoring/grafana/download-dashboards.sh
@@ -6,7 +6,7 @@ for file in alerts-dashboard.json coherence-dashboard-main.json federation-detai
             member-details-dashboard.json proxy-server-detail-dashboard.json services-summary-dashboard.json cache-details-dashboard.json \
             federation-summary-dashboard.json  kubernetes-summary-dashboard.json members-summary-dashboard.json proxy-servers-summary-dashboard.json \
             caches-summary-dashboard.json elastic-data-summary-dashboard.json machines-summary-dashboard.json \
-            persistence-summary-dashboard.json service-details-dashboard.json coherence-executors-summary.json coherence-executor-detail.json cache-store-details-dashboard.json
+            persistence-summary-dashboard.json service-details-dashboard.json executors-summary.json executor-detail.json cache-store-details-dashboard.json
 do
    echo "${file}"
    curl -s https://raw.githubusercontent.com/oracle/coherence-operator/main/dashboards/grafana/${file} -o dashboards/${file}
