64a59040f364cec02d0331a05e7cc1ca6c21b63a
Tim Middleton <tim.middleton@oracle.com>
2022-12-21 13:33:50 +0800

    add topics dashboards

--- monitoring/grafana/download-dashboards.sh
+++ monitoring/grafana/download-dashboards.sh
@@ -6,7 +6,8 @@ for file in alerts-dashboard.json coherence-dashboard-main.json federation-detai
             member-details-dashboard.json proxy-server-detail-dashboard.json services-summary-dashboard.json cache-details-dashboard.json \
             federation-summary-dashboard.json  kubernetes-summary-dashboard.json members-summary-dashboard.json proxy-servers-summary-dashboard.json \
             caches-summary-dashboard.json elastic-data-summary-dashboard.json machines-summary-dashboard.json \
-            persistence-summary-dashboard.json service-details-dashboard.json executors-summary.json executor-detail.json cache-store-details-dashboard.json
+            persistence-summary-dashboard.json service-details-dashboard.json executors-summary.json executor-detail.json cache-store-details-dashboard.json \
+            topic-details-dashboard.json topic-subscriber-details.json topic-subscriber-group-details.json topics-summary-dashboard.json
 do
    echo "${file}"
    curl -s https://raw.githubusercontent.com/oracle/coherence-operator/main/dashboards/grafana/${file} -o dashboards/${file}
